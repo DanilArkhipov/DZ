f1679e6496f620e4d0d2e8cfd307050b3e754c4c
DanilArkhipov <danilarkhipov00@gmail.com>
2020-09-30 14:10:09 +0300

    update

--- FSharpCalculator/FSharpCalc.fs
+++ FSharpCalculator/FSharpCalc.fs
@@ -1,27 +1,63 @@
 ﻿// Learn more about F# at http://fsharp.org
 open System
-let sum a b = a + b;
-let sub a b = a-b;
-let div a b  = a/b;
-    
-let mult  a b = a * b;
+open System
+open System.ComponentModel
+open System.Diagnostics
 
-let calculate firstNumber operation secondNumber =
-    match operation with
-    | "+" -> sum firstNumber secondNumber
-    | "-" -> sub firstNumber secondNumber
-    | "*" -> mult firstNumber secondNumber
-    | "/" -> div firstNumber secondNumber
-    | _ -> raise (SystemException "Invalid input")
-    
-let getDouble input =
-    double input
-    
+let div a b =
+    match b with
+    | 0.0 -> None
+    | _ -> Some(a/b)
+let sum a b =
+    Some(a + b)
+let sub a b =
+    Some(a - b)
+let mult a b =
+    Some(a*b)
+type operation = Sum | Sub |Div |Mult
+let getOperation input =
+    match input with
+    | "+" -> Some(Sum)
+    | "-" -> Some(Sub)
+    | "*" -> Some(Mult)
+    | "/" -> Some(Div)
+    | _ -> None
+let getNum (input:string) =
+    let f,res = Double.TryParse(input)
+    match f with
+    | false -> None
+    | true -> Some(res)
+let showValue x = 
+    match x with
+    |None->Console.Write("Something was wrong!")
+    |Some v -> Console.Write(v.ToString())
+let calculate a op b =
+    match op with
+    | operation.Mult -> Some(mult a b)
+    | operation.Div -> Some(div a b)
+    | operation.Sub -> Some(sub a b)
+    | operation.Sum -> Some(sum a b)
+    | _ -> None
+type MaybeBuilder() =
+
+    member this.Bind(x, f) = 
+        match x with
+        | None -> None
+        | Some a -> f a
+
+    member this.Return(x) = 
+        Some x
+   
+let maybe = new MaybeBuilder()
 [<EntryPoint>]
 let main argv =
-    let firstNumber = getDouble (Console.ReadLine())
-    let operation = Console.ReadLine()
-    let secondNumber = getDouble (Console.ReadLine())
-    calculate firstNumber operation secondNumber |> Console.WriteLine
+    maybe{
+        let! a = getNum (Console.ReadLine())
+        let! b = getOperation (Console.ReadLine())
+        let! c = getNum (Console.ReadLine())
+        let! res = calculate a b c
+        showValue res
+        return res
+    } |> ignore
     0 // return an integer exit code
 
