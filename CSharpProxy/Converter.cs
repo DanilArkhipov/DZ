c45962b7b404abef7bcdc7127ba6df06703e3aab
DanilArkhipov <danilarkhipov00@gmail.com>
2020-10-25 13:04:39 +0300

    Converter Initial State

---
+++ CSharpProxy/Converter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CSharpProxy
+{
+    public static class Converter
+    {
+        internal static Expression Convert()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Queue<string> ToPostfix(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Expression ToExpression(Queue<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
